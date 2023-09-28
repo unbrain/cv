75a85829c0c1ad9c421803c78a5b5213f5c5f85d
刘朝阳 <zhaoyang12@staff.weibo.com>
2023-09-28 15:15:40 +0800

    feat(publish): pic_id

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { siVuedotjs, SimpleIcon } from "simple-icons";
 import weibo from "../assets/weibo.png";
+import me from "../assets/me.jpg";
+import creator from "../assets/creator.jpg";
 import Icon from "../components/Icon.vue";
 interface Project {
   name: string;
@@ -14,29 +16,33 @@ interface Project {
 const arr: Project[] = [
   {
     name: "PC微博",
-    subTitle: "// _ui-animations",
+    subTitle: "发布器",
     picture: weibo,
     icon: siVuedotjs,
     desc: "Duis aute irure dolor in velit esse cillum dolore.",
     link: "https://weibo.com",
   },
   {
-    name: "PC微博",
-    subTitle: "// _ui-animations",
-    picture: "image.png",
-    icon: "vue",
+    name: "pc微博创作者中心",
+    subTitle: "抽奖中心",
+    picture: me,
+    icon: siVuedotjs,
     desc: "Duis aute irure dolor in velit esse cillum dolore.",
-    link: "https://weibo.com",
+    link: "https://me.weibo.com/",
   },
   {
-    name: "PC微博",
-    subTitle: "// _ui-animations",
-    picture: "image.png",
-    icon: "vue",
+    name: "创作者中心",
+    subTitle: "",
+    picture: creator,
+    icon: siVuedotjs,
     desc: "Duis aute irure dolor in velit esse cillum dolore.",
-    link: "https://weibo.com",
+    link: "https://m.weibo.cn/c/wbox?id=qgb2672tlf",
   },
 ];
+
+const openProject = (item: Project) => {
+  window.open(item.link, "_blank");
+};
 </script>
 
 <template>
@@ -52,7 +58,7 @@ const arr: Project[] = [
         <img :src="item.picture" />
         <div class="desc">
           <div class="text">{{ item.desc }}</div>
-          <div class="button">view-project</div>
+          <div class="button" @click="openProject(item)">view-project</div>
         </div>
       </div>
     </div>
@@ -83,7 +89,12 @@ const arr: Project[] = [
     top: 19px;
     right: 19px;
     padding: 5px;
-    background: #86E1F9;
+    background: #86e1f9;
+  }
+  & img {
+    display: inline-block;
+
+    height: 167px;
   }
   & .desc {
     padding: 24px 31px 31px;
@@ -95,6 +106,7 @@ const arr: Project[] = [
       color: #607b96;
     }
     & .button {
+      cursor: pointer;
       text-align: left;
       display: inline-block;
       margin-top: 21px;
