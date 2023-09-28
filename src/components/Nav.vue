1e9147d8b490a9281245b9b89275b2eae4d5909e
unbrain <marsorsun@gmail.com>
2023-09-28 15:15:40 +0800

    feat(publish): pic_id

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,38 +1,69 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { siGnubash } from 'simple-icons';
-import Icon from './Icon.vue';
-import { useRouter } from 'vue-router';
-const router = useRouter()
+import { ref, watch } from "vue";
+import { siGnubash } from "simple-icons";
+import Icon from "./Icon.vue";
+import { useRouter, useRoute } from "vue-router";
+const router = useRouter();
+const route = useRoute();
 const site = {
-  title: '',
-  router: '/'
-}
+  title: "",
+  router: "/",
+};
 
 interface Item {
-  title?: string
-  text?: string,
-  router: string
+  title?: string;
+  text?: string;
+  router: string;
 }
 
-const items = [{ text: '你好', router: 'hi' }, { text: '关于我', router: 'about' }, { text: '项目', router: 'project' }]
+const items = [
+  { text: "你好", router: "hi" },
+  { text: "关于我", router: "about" },
+  { text: "项目", router: "project" },
+];
 
 const cur = ref(0);
 
 const go = (item: Item, index: number) => {
   cur.value = index;
-  router.push({ path: item.router })
-}
+  router.push({ path: item.router });
+};
 
+const handlePath = () => {
+  if (route.path) {
+    for (let i in items) {
+      if (route.path.includes(items[i].router)) {
+        cur.value = +i;
+        break;
+      }
+    }
+  }
+};
+
+watch(
+  () => route.path,
+  () => {
+    handlePath();
+  }
+);
 </script>
 
 <template>
   <div class="flex nav justify-between">
-    <div class="flex ">
-      <div class="flex justify-center items-center mar line site" @click="go(site, 0)">
+    <div class="flex">
+      <div
+        class="flex justify-center items-center mar line site"
+        @click="go(site, 0)"
+      >
         <Icon :icon="siGnubash" :unset="true" />{{ site.title }}
       </div>
-      <div class="mar line item" v-for="(item, index) in items" :key="index" @click="go(item, index)">{{ item.text }}
+      <div
+        class="mar line item"
+        v-for="(item, index) in items"
+        :key="index"
+        @click="go(item, index)"
+      >
+        {{ item.text }}
         <div class="cur" v-if="cur === index"></div>
       </div>
     </div>
@@ -46,7 +77,7 @@ const go = (item: Item, index: number) => {
   top: 0;
   left: 0;
   width: 100%;
-  border-bottom: 1px solid #1E2D3D;
+  border-bottom: 1px solid #1e2d3d;
 
   & .site {
     width: 311px;
@@ -63,13 +94,11 @@ const go = (item: Item, index: number) => {
   }
 
   & .line {
-    border-right: 1px solid #1E2D3D;
+    border-right: 1px solid #1e2d3d;
   }
 
   & .line1 {
-
-    border-left: 1px solid #1E2D3D;
-
+    border-left: 1px solid #1e2d3d;
   }
 
   & .cur {
